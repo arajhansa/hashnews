5babd6b34f5597d5c8114d3bc5fe66d68451fde6
Advay Rajhansa <rajhansa.advay@gmail.com>
2021-04-23 23:58:03 +0530

    Added api calls to service

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,19 +1,26 @@
+import axios from "axios";
 import React from "react";
 
 export default function SearchBar() {
 
   const inputRef = React.createRef()
 
-  function onKeyPressed(event) {
+  async function onKeyPressed(event) {
     if(event.key === 'Enter') {
-      console.log(inputRef.current.value)
+      const word = inputRef.current.value 
+      const response = await axios.get(`https://wordsapiv1.p.rapidapi.com/words/${word}/definitions`, {
+        headers: {
+          "X-RapidAPI-Key": "HCvewCUcStmshIRVdmehbQUen56Pp1dGrCNjsnwesEfsBmUcKW"
+        }
+      })
+      console.log(response.data.definitions[0].definition)
     }
   }
 
   return (
     <div className="px-20 py-6">
       <input ref={inputRef}
-       className="w-1/2 p-4 bg-transparent text-8xl font-bold font-display text-white focus:outline-none border-b-2 border-gray-700"
+       className="w-1/2 p-4 bg-transparent text-8xl font-bold font-display text-white focus:outline-none border-b-2 border-gray-800"
        type="text" maxLength={10} autoFocus={true} onKeyPress={onKeyPressed} />
     </div> 
   );
